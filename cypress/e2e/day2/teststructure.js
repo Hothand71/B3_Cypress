d3dbf03f9641f804becaa0c7a80b4812f089a1ff
Mustafa Kemal <kemalaltun7108@gmail.com>
2023-07-24 00:29:20 +0300

    testsructure

---
+++ cypress/e2e/day2/teststructure.js
@@ -0,0 +1,30 @@
+describe('First Test Suit',function(){
+
+    beforEach(()=>{
+        cy.visit("")
+    })
+
+
+    it("First Test Case",function(){
+        //syntax
+    })
+    it("Second Test Case",function(){
+        //syntax
+    })
+    it("Third Test Case",function(){
+        //syntax
+    })
+
+})
+describe("Second TEst suit",()=>{
+    it("First Test Case",()=>{
+        //syntax
+    })
+    it("Second Test Case",()=>{
+        //syntax
+    })
+    it("Third Test Case",()=>{
+        //syntax
+    })
+}
+)
